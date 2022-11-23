76b008af71979156858ea126603bb8dc2d43ea2e
ryan.o'rourke <ryan.o'rourke@kainos.com>
2022-11-23 12:01:07 +0000

    change getJobRoles function to obtain capability aswell as name

--- app/service/JobService.js
+++ app/service/JobService.js
@@ -1,7 +1,7 @@
 const axios = require('axios');
 axios.defaults.baseURL = process.env.API_URL;
 
-module.exports.URL = 'http://localhost:8080/hr/job-roles';
+module.exports.URL = 'http://localhost:8080/hr/job-roles'; // the base path will need changed probably when docker is used
 
 // get the all of the job roles available
 module.exports.getJobRoles = async function () {
