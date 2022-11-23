63d1a48e2d87b969728d2df4fed4e24c68f5e5b4
ryan.o'rourke <ryan.o'rourke@kainos.com>
2022-11-23 14:53:46 +0000

    add error msg to ui and use env variable for base api url

--- app/service/JobService.js
+++ app/service/JobService.js
@@ -1,15 +1,11 @@
 const axios = require('axios');
 axios.defaults.baseURL = process.env.API_URL;
 
-module.exports.URL = 'http://localhost:8080/hr/job-roles';
+module.exports.URL =  process.env.API_URL + '/hr/job-roles';
 
 // get the all of the job roles available
 module.exports.getJobRoles = async function () {
-    try {
-        // will get a list of job roles from backend db
-        const response = await axios.get(this.URL)
-        return response.data;
-    }catch (e) {
-        return new Error('Could not get job roles')
-    }
+    // will get a list of job roles from backend db
+    const response = await axios.get(this.URL);
+    return response.data;
 }
