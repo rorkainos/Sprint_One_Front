654416d30da71ef4fefbfebb74b48ee9975b790d
ryan.o'rourke <ryan.o'rourke@kainos.com>
2022-11-23 15:00:46 +0000

    remove useless code

--- app/service/JobService.js
+++ app/service/JobService.js
@@ -1,11 +1,10 @@
 const axios = require('axios');
 axios.defaults.baseURL = process.env.API_URL;
 
-module.exports.URL =  process.env.API_URL + '/hr/job-roles';
+module.exports.URL =  '/hr/job-roles';
 
-// get the all of the job roles available
+// get all of the job roles available
 module.exports.getJobRoles = async function () {
-    // will get a list of job roles from backend db
     const response = await axios.get(this.URL);
     return response.data;
 }
