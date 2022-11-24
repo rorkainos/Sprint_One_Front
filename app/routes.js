ea1680ba2877ece0f1ebb96e289a1d3d14f72f22
ryan.o'rourke <ryan.o'rourke@kainos.com>
2022-11-24 09:56:50 +0000

    Revert "001 view job roles (#3)"
    This reverts commit 4cf2d00a76d6ba285bb3861977ceb2537ab8e457.

--- app/routes.js
+++ app/routes.js
@@ -2,19 +2,5 @@ const express = require('express')
 const router = express.Router()
 
 // Add your routes here - above the module.exports line
-const JobService = require('./service/JobService');
-
-// render the jobroles.html page and pass in a list of job roles
-router.get('/jobroles', async (req, res) => {
-    let data = [];
-
-    try {
-       data = await JobService.getJobRoles();
-    } catch (e) {
-        res.locals.errormessage = "An error occured when retrieving the list of Job Roles"
-    }
-    
-    res.render('jobroles', { jobroles: data } ) 
-});
 
 module.exports = router
