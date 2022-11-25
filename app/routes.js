f132b13322a90f31554cd53a8568fd917b988a1c
ryan.o'rourke <ryan.o'rourke@kainos.com>
2022-11-25 09:54:09 +0000

    add jobSpec ui and route from JobRoles to that page
    TODOs have been placed to inform remaining requirements

--- app/routes.js
+++ app/routes.js
@@ -17,4 +17,24 @@ router.get('/jobroles', async (req, res) => {
     res.render('jobroles', { jobroles: data } ) 
 });
 
+// render the jobSpec Page with id and name passed in the request
+router.get('/jobspec', async (req, res) => {
+    let id = req.id;
+    let name = req.name;
+
+    //TODO find a way to pull the id and name from the request
+
+    try {
+        jobSpecification = await JobService.getJobSpecification(id);
+    } catch (e) {
+        if (name){
+            res.locals.errormessage = "An error occured when retrieving the job specification for " + name;
+        }else{
+            res.locals.errormessage = "An error occured when retrieving the job specification";
+        }       
+    }
+    
+    res.render('jobSpec', { jobSpecification: jobSpecification } ) 
+});
+
 module.exports = router
--- app/service/JobService.js
+++ app/service/JobService.js
@@ -2,6 +2,7 @@ const axios = require('axios');
 axios.defaults.baseURL = process.env.API_URL;
 
 module.exports.GET_JOB_ROLES =  '/hr/job-roles';
+module.exports.GET_JOB_SPEC =  '/job-specification/';
 
 // get all of the job roles available
 module.exports.getJobRoles = async function () {
@@ -10,5 +11,15 @@ module.exports.getJobRoles = async function () {
         return response.data;
     }catch{
         return new Error('Could not get Job Roles')
+    }
+}
+
+module.exports.getJobSpecification = async function (jobID) {
+    try{
+        //TODO create tests
+        const response = await axios.get(this.GET_JOB_SPEC + jobID);
+        return response.data;
+    }catch{
+        return new Error('Could not get Job Specification')
     }
 }
