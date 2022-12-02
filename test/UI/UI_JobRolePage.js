36e638a0eb5089fc3c4481a8fb220c838e47e2c5
Mateusz de Martini-Dwojak <mateusz.demartinidwojak@kainos.com>
2022-12-02 10:17:40 +0100

    Add folder test/UI
    Add first selenium case JobRolesPage.js

---
+++ test/UI/UI_JobRolePage.js
@@ -0,0 +1,41 @@
+var axios = require('axios');
+var MockAdapter = require('axios-mock-adapter');
+var chai = require('chai');  
+const expect = chai.expect;
+
+const { Builder, By, Key, until } = require('selenium-webdriver')
+const assert = require('assert');
+const { threadId } = require('worker_threads');
+const { delay } = require('lodash');
+const { resolve } = require('path');
+const { WebDriver } = require('selenium-webdriver');
+
+describe('JobRolesCheck', function() {
+  this.timeout(30000)
+  let driver
+  let vars
+  beforeEach(async function() {
+    driver = await new Builder().forBrowser('chrome').build()
+    vars = {}
+  })
+  afterEach(async function() {
+    await driver.quit();
+  })
+  it('JobRolesCheck', async function() {
+
+    // Go to main Job Roles page
+    await driver.get("http://localhost:3000/jobroles");
+
+    // Wait for 2s for screen record to capture test
+    await driver.sleep(2000);
+
+    // Confirm that it is main page by asserting heading
+    assert(await driver.findElement(By.xpath("/html[1]/body[1]/h2[1]")).getText() == "List of Job Roles");
+    console.log("LOG asserting Header of the page: OK");
+
+    // Confirm that main page table exist by asserting heading
+    assert(await driver.findElement(By.xpath("/html[1]/body[1]/table[1]/thead[1]/tr[1]/th[1]")).getText() == "Name");
+    console.log("LOG asserting Name column header: OK");
+
+  })
+})
