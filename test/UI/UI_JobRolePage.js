959cb93fb302c811a65cadc572bb8df9c907bee6
Mateusz de Martini-Dwojak <mateusz.demartinidwojak@kainos.com>
2022-12-05 14:41:30 +0100

    Updated test/UI/UI_JobRolePage.js
    Changed name of method, added validation of table element on webpage

--- test/UI/UI_JobRolePage.js
+++ test/UI/UI_JobRolePage.js
@@ -3,12 +3,14 @@ var MockAdapter = require('axios-mock-adapter');
 var chai = require('chai');  
 const expect = chai.expect;
 
-const { Builder, By, Key, until } = require('selenium-webdriver')
+const { Builder, By, Key, until, Browser } = require('selenium-webdriver')
 const assert = require('assert');
 const { threadId } = require('worker_threads');
 const { delay } = require('lodash');
 const { resolve } = require('path');
 const { WebDriver } = require('selenium-webdriver');
+const { default: isDisplayed } = require('webdriverio/build/commands/element/isDisplayed');
+const e = require('express');
 
 describe('JobRolesCheck', function() {
   this.timeout(30000)
@@ -22,7 +24,7 @@ describe('JobRolesCheck', function() {
     await driver.close();
     await driver.quit();
   })
-  it('Asserting that heading List of Job Roles exists', async function() {
+  it('Asserting that heading List of Job Roles exists, asserting Job Roles table exist', async function() {
 
     // Go to main Job Roles page
     await driver.get("http://localhost:3000/jobroles");
@@ -34,8 +36,21 @@ describe('JobRolesCheck', function() {
     assert.equal(await driver.findElement(By.xpath("/html[1]/body[1]/h2[1]")).getText(), "List of Job Roles");
     console.log("LOG asserting Header of the page: OK");
 
-    // Confirm that main page table exist by asserting heading
-    assert.equal(await driver.findElement(By.id("Name")).getText(), "Name");
-    console.log("LOG asserting Name column header: OK");
+    // Checking if Job Roles table element exists on a page
+    try {
+      const table = await driver.findElement(By.id("job-role-table"));
+      console.log("LOG Job Roles table is displayed on main page");
+
+      if(table != null || table != undefined){
+        console.log("LOG Job Roles table is not null/undefined");
+      } else {
+        assert.fail("LOG missing Job Roles table on a page");
+      }
+
+    }
+    catch(NoSuchElementError) {
+      assert.fail("LOG missing Job Roles table");
+    }
+
   })
 })
