07a3d445801bbd8b18318ba9117a777f7335c436
james-walshe <117290173+james-walshe@users.noreply.github.com>
2022-12-07 13:57:07 +0000

    012 add role (#9)
    add create a new job role ui feature

--- app/routes.js
+++ app/routes.js
@@ -1,4 +1,6 @@
 const express = require('express')
+const JobRoleValidator = require('./validator/JobRoleValidator')
+const js = require('./service/JobService')
 const router = express.Router()
 
 // Add your routes here - above the module.exports line
@@ -9,12 +11,65 @@ router.get('/jobroles', async (req, res) => {
     let data = [];
 
     try {
-       data = await JobService.getJobRoles();
+        data = await JobService.getJobRoles();
     } catch (e) {
         res.locals.errormessage = "An error occured when retrieving the list of Job Roles"
     }
-    
-    res.render('jobroles', { jobroles: data } ) 
+
+    res.render('jobroles', { jobroles: data })
+});
+
+// render the addjobroles.html page 
+router.get('/addjobrole', async (req, res) => {
+
+    // get data for populating job family and band level dropdowns
+    let data = await JobService.getJobRoleInfo();
+    // render form page
+    res.render('addjobrole', { formData: data });
+
+});
+
+// render the addjobroles.html page 
+router.post('/addjobrole', async (req, res) => {
+
+    // validate job role
+    let error = new JobRoleValidator.validateJobRole(req.body);
+
+    if (Object.keys(error).length !== 0) {
+        // get data for populating job family and band level dropdowns
+        let data = await JobService.getJobRoleInfo();
+ 
+        // parse job family and band level data passed from form into JSON
+        if (req.body.jobFamily != '') {
+            req.body.jobFamily = JSON.parse(req.body.jobFamily)
+        }
+        if (req.body.bandLevel != '') {
+            req.body.bandLevel = JSON.parse(req.body.bandLevel)
+        }
+
+        // render page again with errors and previous form data
+        res.render('addjobrole', { error: error, data: req.body, formData: data })
+    } else {
+
+        // retain form data in case insert fails
+        let insertData = req.body;
+
+        // remove names from job family and band level fields in response data
+        req.body.jobFamily = JSON.parse(req.body.jobFamily).jobFamilyID;
+        req.body.bandLevel = JSON.parse(req.body.bandLevel).bandLevelID;
+
+        try {
+            // insert new job
+            await js.insertJobRole(req.body)
+            // redirect to job roles page
+            let data = await JobService.getJobRoles()
+            res.render('jobroles', {inserted:true, jobroles: data})
+        } catch (e) {
+            // render form again with insertion error displayed
+            let error = { "insertError": "Could not insert new job role, please try again." }
+            res.render('addjobrole', { error: error, data: req.body, formData: insertData })
+        }
+    }
 });
 
 // render the jobSpec Page with id and name passed in the request
--- app/service/JobService.js
+++ app/service/JobService.js
@@ -1,16 +1,42 @@
 const axios = require('axios');
 axios.defaults.baseURL = process.env.API_URL;
 
-module.exports.GET_JOB_ROLES =  '/hr/job-roles';
+module.exports.JOB_ROLE_ENDPOINT = '/hr/job-roles';
+module.exports.GET_JOB_ROLE_INFO = '/hr/job-role-info';
 module.exports.GET_JOB_SPEC =  '/hr/job-specification/';
+module.exports.axios = axios;
 
 // get all of the job roles available
 module.exports.getJobRoles = async function () {
-    try{
-        const response = await axios.get(this.GET_JOB_ROLES);
+    try {
+        const response = await axios.get(this.JOB_ROLE_ENDPOINT);
         return response.data;
-    }catch{
-        throw new Error('Could not get Job Roles');
+    } catch {
+        throw new Error('Could not get Job Roles.')
+    }
+}
+
+// insert new job role
+module.exports.insertJobRole = async function (data) {
+    try {
+        // post request to add new job role
+        const response = await axios.post(this.JOB_ROLE_ENDPOINT, data);
+        return response;
+    } catch {
+        // throw exception if call fails
+        throw new Error('Could not create new Job Roles.')
+    }
+}
+
+module.exports.getJobRoleInfo = async function () {
+    try {
+        // get request to get job family and band level data
+        const response = await axios.get(this.GET_JOB_ROLE_INFO);
+        return response.data;
+
+    } catch {
+        // throw exception if call fails
+        throw new Error('Could not get Job Role info.')
     }
 }
 
@@ -20,5 +46,6 @@ module.exports.getJobSpecification = async function (jobID) {
         return response.data;
     }catch{ 
         throw new Error('Could not get Job Specification');
+
     }
 }
